57143fe714c5718cf259b1a93d9ac669013d6f79
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-22 16:01:10 +0100

    refactoring the interactive menu

--- directory.rb
+++ directory.rb
@@ -1,30 +1,20 @@
 def interactive_menu
-students = []
+@students = []
 loop do
   # 1. Print the menu and ask the user what to do
+  # 2. read the input and save it into a variable
+  # 3. Do what the user has asked
+  print_menu
+  process(gets.chomp)
+end
+end
+
+#let's put all the students into an array
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit" # 9 because we'll be adding more items
-  # 2. read the input and save it into a variable
-  selection = gets.chomp
-  # 3. Do what the user has asked
-  case selection
-  when "1"
-    students = input_students
-  when "2"
-    print_header
-    print(students)
-    print_footer(students)
-  when "9"
-    exit # this will cause the program to terminate
-  else
-    puts "i don't know what you meant, try again"
-  end
 end
-end
-
-
-#let's put all the students into an array
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -47,7 +37,7 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
+def print_students_list(students)
   students.each do |student|
     puts "#{student[:name]} (#{:cohort} :november)"
   end
@@ -55,5 +45,22 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
 #nothing happens until we call the methods
+def process(selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "i don't know what you meant, try again"
+  end
+end
 interactive_menu
