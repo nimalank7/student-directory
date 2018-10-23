c44585a4a6ca79232b99a3f8bc1a1f757c74706b
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-23 12:33:37 +0100

    added arguments from command line

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ loop do
   # 2. read the input and save it into a variable
   # 3. Do what the user has asked
   print_menu
-  process(gets.chomp)
+  process(STDIN.gets.chomp)
 end
 end
 
@@ -23,14 +23,14 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   # return the array of students
   students
@@ -63,13 +63,24 @@ def save_students
   end
   file.close
 end
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 #nothing happens until we call the methods
@@ -90,3 +101,4 @@ def process(selection)
   end
 end
 interactive_menu
+try_load_students
