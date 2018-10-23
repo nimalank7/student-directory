4c88f1065756a174c9cdcf6cb518dd45fba2bc57
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-23 11:38:23 +0100

    added a loading students method

--- directory.rb
+++ directory.rb
@@ -14,6 +14,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 def input_students
@@ -62,6 +63,15 @@ def save_students
   end
   file.close
 end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 #nothing happens until we call the methods
 def process(selection)
   case selection
@@ -71,6 +81,8 @@ def process(selection)
     save_students
   when "2"
     show_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
