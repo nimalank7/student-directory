115c91337b97965bf94a40a4f47a76d5009f63cf
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-25 15:28:54 +0100

    q5

--- directory.rb
+++ directory.rb
@@ -1,18 +1,20 @@
 #let's put all the students into an array
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students and their hobbies"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  hobby = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while !name.empty? && !hobby.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    hobby = gets.chomp
   end
   # return the array of students
   students
@@ -22,10 +24,8 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  accumulator = 0
-  while (accumulator < students.length)
-    puts "#{students[accumulator][:name]} (#{students[accumulator][:cohort]} cohort)"
-    accumulator += 1
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort) hobby: #{student[:hobby]}"
   end
 end
 def print_footer(names)
