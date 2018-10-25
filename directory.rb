68bd21320e1a7efaeece75c76b9b97ae3f980197
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-25 15:33:12 +0100

    q6

--- directory.rb
+++ directory.rb
@@ -1,35 +1,33 @@
 #let's put all the students into an array
 def input_students
-  puts "Please enter the names of the students and their hobbies"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students and their hobbies".center(100)
+  puts "To finish, just hit return twice".center(100)
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  hobby = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? && !hobby.empty? do
+  while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students".center(100)
     # get another name from the user
     name = gets.chomp
-    hobby = gets.chomp
   end
   # return the array of students
   students
 end
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(100)
+  puts "-------------".center(100)
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort) hobby: #{student[:hobby]}"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
   end
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(100)
 end
 #nothing happens until we call the methods
 students = input_students
